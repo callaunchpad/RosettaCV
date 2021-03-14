4face7dc40e2d39f4908024395acc99eefebf6c3
Sean O'Brien <sean.obrien@berkeley.edu>
2021-03-14 12:23:29 -0700

    small changes to clean up trainer

--- src/train.py
+++ src/train.py
@@ -2,31 +2,32 @@ import torch
 import torch.nn as nn
 import wandb
 
-from models.DenoisingAE import Encoder, Decoder, DenoisingAE
 from data_loader.data_loaders import FashionMnistDenoising
-
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def train(model, num_epochs=10, batch_size=32, learning_rate=1e-3):
-    print("[*] Training DenoisingAE on FashionMNIST")
-    fashion_mnist_denoise = FashionMnistDenoising()
-    torch.manual_seed(7)
-    criterion = fashion_mnist_denoise.loss_fn()
+WANDB = False
+
+def train(model,
+          task,
+          num_epochs: int=10,
+          batch_size: int=32,
+          learning_rate: float=1e-3,
+          noise_ratio: float=0.15,
+          seed=None):
+    print("[*] Training DenoisingAE")
+    if seed:
+        torch.seed(seed)
+    criterion = task.loss_fn()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
-    train_dl = fashion_mnist_denoise.loader(True, batch_size=32)
+    train_dl = task.loader(train=True, batch_size=32)
 
     for epoch in range(num_epochs):
         for img, _ in train_dl:
             img = img.to(device)
             img = img / 255
             img = img.view(-1, 28, 28)
-
-            noisy_imgs = []
-            for i in range(img.shape[0]):
-                noisy_mask = torch.FloatTensor(28, 28).to(device).uniform_() > 0.15
-                noisy_imgs.append(img[i] * noisy_mask)
-            noisy_imgs = torch.stack(noisy_imgs)
-            noisy_imgs = noisy_imgs.unsqueeze(1).to(device)
+            mask = (torch.rand(*img.shape) > noise_ratio).to(device)
+            noisy_imgs = (img*mask).to(device).unsqueeze(1)
 
             out = model(noisy_imgs)
             img = img.unsqueeze(1)
@@ -35,13 +36,20 @@ def train(model, num_epochs=10, batch_size=32, learning_rate=1e-3):
             optimizer.step()
             optimizer.zero_grad()
 
-        wandb.log({ "epoch": epoch + 1, "loss": float(loss) })
+        if USE_WANDB:
+            wandb.log({ "epoch": epoch + 1, "loss": float(loss) })
         print('Epoch:{} Loss:{:.4f}'.format(epoch+1, float(loss)))
 
-encoder = Encoder().to(device)
-decoder = Decoder().to(device)
-model = DenoisingAE(encoder, decoder).to(device)
+if __name__=="__main__":
+    from models.DenoisingAE import Encoder, Decoder, DenoisingAE
+    encoder = Encoder().to(device)
+    decoder = Decoder().to(device)
+    fashion_mnist_denoise = FashionMnistDenoising()
+    model = DenoisingAE(encoder, decoder).to(device)
 
-with wandb.init(project="DenoisingAE"):
-    train(model)
-    wandb.alert(title="Train DenoisingAE", text="Finished training")
+    if USE_WANDB:
+        with wandb.init(project="DenoisingAE"):
+            train(model, fashion_mnist_denoise)
+            wandb.alert(title="Train DenoisingAE", text="Finished training")
+    else:
+        train(model, fashion_mnist_denoise)
