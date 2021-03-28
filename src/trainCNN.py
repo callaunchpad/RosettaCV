dac55750334c958982863a309e95a58f54ef8529
Elaine Qian <elaineqian@berkeley.edu>
2021-03-28 12:08:03 -0700

    fix transform

--- src/trainCNN.py
+++ src/trainCNN.py
@@ -1,9 +1,10 @@
 import torch.nn as nn
+from torchvision import transforms
 from torchvision.datasets import ImageFolder
 
 from models.CNN import CNN
 from trainer.trainer import Trainer
-from data_loader.few_shot_dataloaders import SubSampledDataset
+import data_loader.few_shot_dataloaders
 
 transform = transforms.Compose([
         transforms.Resize(256),
@@ -11,13 +12,13 @@ transform = transforms.Compose([
         transforms.ToTensor()
     ])
 
-train_set = ImageFolder('/datasets/imagenetwhole/ilsvrc2012/train/', transform=self.transform)
-val_set = ImageFolder('/datasets/imagenetwhole/ilsvrc2012/val/', transform=self.transform)
+train_set = ImageFolder('/datasets/imagenetwhole/ilsvrc2012/train/', transform=transform)
+val_set = ImageFolder('/datasets/imagenetwhole/ilsvrc2012/val/', transform=transform)
 
 model = CNN()
 loss = nn.CrossEntropyLoss()
-train_loader = SubSampledDataset.get_few_shot_dataloader(train_set)
-val_loader = SubSampledDataset.get_few_shot_dataloader(val_set)
+train_loader = get_few_shot_dataloader(train_set)
+val_loader = get_few_shot_dataloader(val_set)
 
 trainer = Trainer(model, loss, train_loader, val_loader)
 trainer.train(epochs=100)
